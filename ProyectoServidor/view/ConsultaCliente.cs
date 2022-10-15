000eb0c34cbc93b3e4ada5b418b11c62e9f6e6cd
Andrey Torrente <68315828+andreyt98@users.noreply.github.com>
2022-10-15 00:08:54 -0600

    Update ConsultaCliente.cs

--- ProyectoServidor/view/ConsultaCliente.cs
+++ ProyectoServidor/view/ConsultaCliente.cs
@@ -6,18 +6,12 @@ namespace ProyectoServidor.view {
         }
 
         private void btnVerDatos_Click(object sender, EventArgs e) {
-<<<<<<< HEAD
-=======
-       // Controllers.ConsultaCliente cc = new(this);
->>>>>>> 7d8c0238e664b1a824bcbdc48eabad2d00b5edb8
+
             BaseDatos baseDatos = new BaseDatos();
 
             clienteDataGrid.Enabled = true;
             clienteDataGrid.DataSource = baseDatos.obtenerClientes();
-<<<<<<< HEAD
-=======
-          //  cc.verDatos(sender, e, clienteDataGrid);
->>>>>>> 7d8c0238e664b1a824bcbdc48eabad2d00b5edb8
+
         }
     }
 }
